9a52557168349dfe63c3333fc5b03285aceeba73
MageeLin <linjingyi321@qq.com>
2022-02-15 10:15:30 +0800

    feat: 翻译modeling-api

--- modeling-api/app/snippets/editingElements.js
+++ modeling-api/app/snippets/editingElements.js
@@ -2,37 +2,37 @@ import diagram from '../../resources/editingElements.bpmn';
 
 function fn(modeler) {
   /**
-   * Now, let's have a look at how you can edit existing elements.
+   * 现在来看一下如何编辑已存在的 elements
    *
-   * The modules used in this example are:
+   * 本示例使用到的模块如下：
    *
-   * * BpmnFactory: Creates new business objects.
-   * * ElementRegistry: A registry of all shapes and connections of the diagram.
-   * * Modeling: The main module for modeling.
+   * * ElementFactory: 创建新的 shape 和 connection.
+   * * ElementRegistry: 图表中所有 shape 和 connection 的仓库.
+   * * Modeling: 进行 modeling 的主模块
    *
-   * We will use these modules to update the properties of two existing shapes.
+   * 我们将会使用这些模块来更新两个已存在 shape 的属性
    */
 
-  // (1) Get the modules
+  // (1) 获取模块
   const bpmnFactory = modeler.get('bpmnFactory'),
         elementRegistry = modeler.get('elementRegistry'),
         modeling = modeler.get('modeling');
 
-  // (2) Get the shapes
+  // (2) 获取 shapes
   const startEvent = elementRegistry.get('StartEvent_1'),
         exclusiveGateway = elementRegistry.get('ExclusiveGateway_1'),
         sequenceFlow = elementRegistry.get('SequenceFlow_3'),
         task = elementRegistry.get('Task_1');
 
-  // (3) Change the start event's `name` property using `updateProperties`
+  // (3) 使用 `updateProperties` 改变 start event 的  `name` 属性
   modeling.updateProperties(startEvent, { name: 'Foo' });
 
-  // (4) Change the `defaultFlow` property of a gateway
+  // (4) 改变一个 gateway 的 `defaultFlow` 属性
   modeling.updateProperties(exclusiveGateway, {
     default: sequenceFlow.businessObject
   });
 
-  // (5) Change a task to be multi-instance
+  // (5) 改变一个 task 为多示例
   const multiInstanceLoopCharacteristics = bpmnFactory.create('bpmn:MultiInstanceLoopCharacteristics');
 
   modeling.updateProperties(task, {
